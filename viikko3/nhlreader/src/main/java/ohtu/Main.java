62f4d1214af97d2001cbe05a2d9ec5a90f93f1e0
Anna Lehto <anviinamaki@gmail.com>
2020-11-09 17:41:54 +0200

    Viikko 3: teht. 2.

--- viikko3/nhlreader/src/main/java/ohtu/Main.java
+++ viikko3/nhlreader/src/main/java/ohtu/Main.java
@@ -7,6 +7,12 @@ package ohtu;
 
 import com.google.gson.Gson;
 import java.io.IOException;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
 import org.apache.http.client.fluent.Request;
 
 public class Main {
@@ -21,13 +27,20 @@ public class Main {
         Gson mapper = new Gson();
         Player[] players = mapper.fromJson(bodyText, Player[].class);
         
-        System.out.println("Oliot:");
-        for (Player player : players) {
-            if (player.getNationality().equals("FIN")) {
-                System.out.println(player);
-            }
-            
-        }   
+        List<Player> listPlayers = new ArrayList<>();
+        Arrays.stream(players)
+            .forEach(player -> {
+                if (player.getNationality().equals("FIN")) {
+                    //listPlayers.add(player);
+                    listPlayers.add(new Player(player.getName(), player.getNationality(), player.getAssists(), player.getGoals(), (player.getAssists() + player.getGoals())));
+                }
+            });
+        System.out.println();
+        System.out.println("Players from FIN " + LocalDate.now());
+        System.out.println();
+        listPlayers.stream()
+            .sorted()
+            .forEach(p -> System.out.println(p.getName() + " " + p.getNationality() + " " + p.getAssists() + " + " + p.getGoals() + " = " + p.getPoints()));
     }
   
 }
